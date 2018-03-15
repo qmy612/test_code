474be6eade094a6666f20b11e461ddf4fefff6d3
qmy612 <jsqianmengyao@163.com>
2018-03-15 23:12:03 +0800

    Create train_vgg.sh

---
+++ train_vgg.sh
@@ -0,0 +1,2 @@
+GLOG_log_dir='/home/z/lunaexp/VGG/log/log9/lr5' \
+/home/z/caffe-master/build/tools/caffe train -solver /home/z/lunaexp/VGG/64/solver.prototxt -weights /home/z/lunaexp/VGG/VGG16-64-27gray-dp45_iter_30000.caffemodel
